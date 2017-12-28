7558a351a23a25200a062c6741af6dfb0b81b5b6
Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>
2017-12-28 10:10:33 +0900

    runscript: fix wrong ENV
    RTE_SDK same as on host is not work properly because path is different
    on container. This update fixes this bug by adding WORKDIR and uses it
    for RTE_SDK. WORKDIR is same value as defned in Dockerfile.
    
    Signed-off-by: Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>

--- run.sh
+++ run.sh
@@ -2,12 +2,13 @@
 
 TARGET=spp
 CMD=bash
+WORKDIR=/usr/src/dpdk
 
 docker-compose run \
 	-e http_proxy=$http_proxy \
 	-e https_proxy=$https_proxy \
 	-e HTTP_PROXY=$http_proxy \
 	-e HTTPS_PROXY=$https_proxy \
-	-e RTE_SDK=$RTE_SDK \
+	-e RTE_SDK=$WORKDIR/dpdk \
 	-e RTE_TARGET=$RTE_TARGET \
 	$TARGET $CMD
