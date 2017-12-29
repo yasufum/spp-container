13001fce638d79bdc0e4c6873a6fa53ef9d35be7
Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>
2017-12-29 16:12:01 +0900

    runscript: add help message
    Signed-off-by: Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>

--- run.sh
+++ run.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 TARGET=spp
-CMD=bash
-WORKDIR=/usr/src/dpdk
+CMD=$1
+#WORKDIR=/usr/src/dpdk
 
-docker-compose run \
-	-e http_proxy=$http_proxy \
-	-e https_proxy=$https_proxy \
-	-e HTTP_PROXY=$http_proxy \
-	-e HTTPS_PROXY=$https_proxy \
-	-e RTE_SDK=$WORKDIR/dpdk \
-	-e RTE_TARGET=$RTE_TARGET \
-	$TARGET $CMD
+if [ ! $1 ]; then
+  echo "usage: $0 [command]"
+  exit
+fi
+
+sudo docker-compose run \
+	-e http_proxy=${http_proxy} \
+	-e https_proxy=${https_proxy} \
+	-e HTTP_PROXY=${http_proxy} \
+	-e HTTPS_PROXY=${https_proxy} \
+	-e RTE_SDK=${WORKDIR}/dpdk \
+	-e RTE_TARGET=${RTE_TARGET} \
+	${TARGET} ${CMD}
