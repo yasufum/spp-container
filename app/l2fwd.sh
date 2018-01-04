999bdeefd01c11cdbac749d63d3ce653d5550d5a
Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>
2018-01-04 15:46:37 +0900

    app: refactor l2fwd.sh
    Signed-off-by: Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>

--- app/l2fwd.sh
+++ app/l2fwd.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+
+APP_DIR=`dirname ${0}`
 CONTAINER_NAME=spp-container
+
+# Include env.sh
+. ${APP_DIR}/../env.sh
+
+CMD=${RTE_SDK}/examples/l2fwd/${RTE_TARGET}/l2fwd
+
 DEV_ID1=1
 DEV_ID1=2
+CORELIST=4-5
+PORTMASK=0x03
+MEM=1024
 
 # vhost device
 sock_host1=/tmp/sock${DEV_ID1}
@@ -10,14 +21,17 @@ sock_guest1=/var/run/usvhost${DEV_ID1}
 sock_host2=/tmp/sock${DEV_ID2}
 sock_guest2=/var/run/usvhost${DEV_ID2}
 
-sudo docker run -i -t \
+cd ${APP_DIR}; \
+  sudo docker run -i -t \
   -v ${sock_host1}:${sock_guest1} \
   -v ${sock_host2}:${sock_guest2} \
   -v /dev/hugepages:/dev/hugepages \
   ${CONTAINER_NAME} \
-  /usr/src/dpdk/dpdk/examples/l2fwd/build/app/l2fwd -l 4-5 -n 4 -m 1024 \
+  ${CMD} -l ${CORELIST} \
+  -n 4 \
+  -m ${MEM}\
   --vdev=virtio_user1,path=${sock_guest1} \
   --vdev=virtio_user2,path=${sock_guest2} \
   --file-prefix=container \
   -- \
-  -p 0x03
+  -p ${PORTMASK}
