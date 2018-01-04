9c22f47edb9e6cc481ec8ea35a94088d21b27963
Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>
2018-01-04 15:47:18 +0900

    app: change testpmd to use one port
    Launching testpmd with two virtio device failed to activate second port
    and cannot forward.
    
    This update is to change testpmd.sh to use only one port. Use two
    process of testpmd if you do forwarding instead of one process with
    two ports.
    
    Signed-off-by: Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>

--- app/testpmd.sh
+++ app/testpmd.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
+CORELIST=$2
+MEM=1024
+
 CONTAINER_NAME=spp-container
-DEV_ID1=1
-DEV_ID1=2
+DEV_ID=$1
+
+APP_DIR=`dirname ${0}`
 
 # vhost device
-sock_host1=/tmp/sock${DEV_ID1}
-sock_guest1=/var/run/usvhost${DEV_ID1}
-sock_host2=/tmp/sock${DEV_ID2}
-sock_guest2=/var/run/usvhost${DEV_ID2}
+sock_host1=/tmp/sock${DEV_ID}
+sock_guest1=/var/run/usvhost${DEV_ID}
 
-sudo docker run -i -t \
+cd ${APP_DIR}; \
+  sudo docker run -i -t \
   -v ${sock_host1}:${sock_guest1} \
-  -v ${sock_host2}:${sock_guest2} \
   -v /dev/hugepages:/dev/hugepages \
   ${CONTAINER_NAME} \
-  testpmd -l 4-5 -n 4 -m 1024 --no-pci \
-  --vdev=virtio_user1,path=${sock_guest1} \
-  --vdev=virtio_user2,path=${sock_guest2} \
-  --file-prefix=container \
+  testpmd -l ${CORELIST} -n 4 -m ${MEM} \
+  --no-pci \
+  --vdev=virtio_user${DEV_ID},path=${sock_guest1} \
+  --file-prefix=container${DEV_ID} \
   -- -i \
   --txqflags=0xf00 --disable-hw-vlan
