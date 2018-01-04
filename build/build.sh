11b710cad592006a4725fe490746c43b9317e90d
Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>
2018-01-04 13:30:37 +0900

    build: make Dockerfile import env.sh
    To share environments among build script and container lancher
    scripts, create env.sh in project home directory.
    
    Build script copies env.sh to build/ and use it in Dockerfile
    to copy it in created image.
    
    env.sh is also used in container launchers.
    
    Signed-off-by: Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
 TARGET=spp-container
-DIR=.
+DIR=`dirname ${0}`
 
-sudo docker build -t $TARGET $DIR
+cp ${DIR}/../env.sh ${DIR}/
+
+if [ ! -e env.sh ]; then
+  echo "Error: env.sh doesn't exist!"
+  exit
+else
+  sudo docker build -t $TARGET $DIR
+  rm ${DIR}/env.sh
+fi
