6e33bf76831d30d634677ff0e5fee8e3be41a61b
Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>
2018-01-07 16:15:58 +0900

    app: remove runcmd from docker-compose
    Signed-off-by: Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>

--- app/run.sh
+++ app/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-APP_DIR=`dirname ${0}`
-TARGET=runcmd
 CMD=$1
+APP_DIR=`dirname ${0}`
+CONTAINER_NAME=spp-container
 
 # Include env.sh
 . ${APP_DIR}/../env.sh
@@ -13,11 +13,12 @@ if [ ! $1 ]; then
 fi
 
 cd ${APP_DIR}; \
-  sudo docker-compose run \
+  sudo docker run -i -t \
 	-e http_proxy=${http_proxy} \
 	-e https_proxy=${https_proxy} \
 	-e HTTP_PROXY=${http_proxy} \
 	-e HTTPS_PROXY=${https_proxy} \
 	-e RTE_SDK=${RTE_SDK} \
 	-e RTE_TARGET=${RTE_TARGET} \
-	${TARGET} ${CMD}
+	${CONTAINER_NAME} \
+  ${CMD}
