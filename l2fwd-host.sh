bce2ea77d3f4450311b7938ab245a722f59b92cb
Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>
2018-01-09 13:37:45 +0900

    app: fix bug
    Fix inapropriate argument.
    
    * app/spp-nfv-vhost.sh
    * l2fwd-host.sh
    
    Signed-off-by: Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>

--- l2fwd-host.sh
+++ l2fwd-host.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 COREMASK=0x0f
-PORTMASK=0x0f
+PORTMASK=0x03
 MEM=1024
 
 SOCK_ID1=0
@@ -14,6 +14,7 @@ sudo $RTE_SDK/examples/l2fwd/build/app/l2fwd \
   -c ${COREMASK} -n 4 \
   --socket-mem ${MEM} \
   --proc-type auto \
+  -b 2a:00.0 -b 2a:00.1 \
   --file-prefix l2fwd-host \
   --vdev eth_vhost${SOCK_ID1},iface=/tmp/sock${SOCK_ID1},queues=2 \
   --vdev eth_vhost${SOCK_ID2},iface=/tmp/sock${SOCK_ID2},queues=2 \
