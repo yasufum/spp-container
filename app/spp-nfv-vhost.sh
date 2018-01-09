227128b27fb313ca92a794eaeaa3dff10e5b3943
Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>
2018-01-09 11:26:12 +0900

    app: refactor variable names
    Signed-off-by: Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>

--- app/spp-nfv-vhost.sh
+++ app/spp-nfv-vhost.sh
@@ -2,10 +2,10 @@
 
 SEC_ID=$1
 CORELIST=$2
-DEV_ID=0
+DEV_ID=$3
 
 MEM=1024
-CTRL_IP=$3
+CTRL_IP=$4
 CTRL_PORT=6666
 
 CONTAINER_NAME=spp-container
@@ -16,20 +16,20 @@ APP_DIR=`dirname ${0}`
 . ${APP_DIR}/../env.sh
 
 # vhost device
-sock_host1=/tmp/sock${DEV_ID}
-sock_guest1=/var/run/usvhost${DEV_ID}
+sock_host=/tmp/sock${DEV_ID}
+sock_guest=/var/run/usvhost${DEV_ID}
 
 CMD=${RTE_SDK}/../spp/src/nfv/${RTE_TARGET}/spp_nfv
 
 cd ${APP_DIR}; \
   sudo docker run -i -t \
-  -v ${sock_host1}:${sock_guest1} \
+  -v ${sock_host}:${sock_guest} \
   -v /dev/hugepages:/dev/hugepages \
   -v /var/run/:/var/run/ \
   ${CONTAINER_NAME} \
   ${CMD} \
   -l ${CORELIST} -n 4 -m ${MEM} \
-  --vdev=virtio_user${DEV_ID},path=${sock_guest1} \
+  --vdev=virtio_user${DEV_ID},path=${sock_guest} \
   --proc-type=secondary \
   -- \
   -n ${SEC_ID} \
--- app/spp-vm.sh
+++ app/spp-vm.sh
@@ -2,10 +2,10 @@
 
 SEC_ID=$1
 CORELIST=$2
-DEV_ID=$3
+DEV_ID=$4
 
 MEM=1024
-CTRL_IP=$4
+CTRL_IP=$3
 CTRL_PORT=6666
 
 CONTAINER_NAME=spp-container
@@ -16,21 +16,21 @@ APP_DIR=`dirname ${0}`
 . ${APP_DIR}/../env.sh
 
 # vhost device
-sock_host1=/tmp/sock${DEV_ID}
-sock_guest1=/var/run/usvhost${DEV_ID}
+sock_host=/tmp/sock${DEV_ID}
+sock_guest=/var/run/usvhost${DEV_ID}
 
 CMD=${RTE_SDK}/../spp/src/vm/${RTE_TARGET}/spp_vm
 
 cd ${APP_DIR}; \
   sudo docker run -i -t \
-  -v ${sock_host1}:${sock_guest1} \
+  -v ${sock_host}:${sock_guest} \
   -v /dev/hugepages:/dev/hugepages \
   -v /var/run/:/var/run/ \
   ${CONTAINER_NAME} \
   ${CMD} \
   -l ${CORELIST} -n 4 -m ${MEM} \
   --proc-type=primary \
-  --vdev=virtio_user${DEV_ID},path=${sock_guest1} \
+  --vdev=virtio_user${DEV_ID},path=${sock_guest} \
   --file-prefix=spp-container0 \
   -- \
   -n ${SEC_ID} \
