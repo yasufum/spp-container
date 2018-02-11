e46790f471c2a4114f26a1cc81b50264b53c282e
Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>
2018-02-11 22:26:31 +0900

    app: update testpmd.py to accept multiple devices
    Change '--dev-id' to '--dev-ids' for multiple devices.
    
    Example:
      $ testpmd.py ... -dev-ids 1,3-5
    
    Signed-off-by: Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>

--- app/testpmd.py
+++ app/testpmd.py
@@ -22,15 +22,15 @@ def parse_args():
         '-m', '--mem',
         type=int,
         default=1024,
-        help="Memory size for spp_nfv")
+        help="Memory size for spp_nfv, exp: -m 1024")
     parser.add_argument(
         '--socket-mem',
         type=str,
-        help="Memory size for spp_nfv")
+        help="Memory size for spp_nfv, exp: --socket-mem 512,512")
     parser.add_argument(
-        '-d', '--dev-id',
-        type=int,
-        help="vhost device ID")
+        '-d', '--dev-ids',
+        type=str,
+        help="vhost device IDs")
     parser.add_argument(
         '-n', '--nof-memchan',
         type=int,
@@ -41,12 +41,25 @@ def parse_args():
         type=str,
         default='spp-container',
         help="Name of container image")
+    parser.add_argument(
+        '--pci',
+        action='store_true',
+        help="Enable PCI (default is None)")
+    parser.add_argument(
+        '--enable-hw-vlan',
+        action='store_true',
+        help="Enable hardware vlan (default is None)")
+    parser.add_argument(
+        '--dry-run',
+        action='store_true',
+        help="Print matrix for checking and exit. Do not run testpmd")
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
 
+    # Check core_mask or core_list is defined.
     if args.core_mask is not None:
         core_opt = {'attr': '-c', 'val': args.core_mask}
     elif args.core_list is not None:
@@ -54,44 +67,77 @@ def main():
     else:
         common.error_exit('--core-mask or --core-list')
 
+    # Check memory option is defined.
     if args.socket_mem is not None:
         mem_opt = {'attr': '--socket-mem', 'val': args.socket_mem}
     else:
         mem_opt = {'attr': '-m', 'val': str(args.mem)}
 
-    if args.dev_id is None:
-        common.error_exit('--dev-id')
+    # Check for other mandatory opitons.
+    if args.dev_ids is None:
+        common.error_exit('--dev-ids')
 
-    sock_host = '/tmp/sock%d' % args.dev_id
-    sock_guest = '/var/run/usvhost%d' % args.dev_id
+    # Setup for vhost devices with given device IDs.
+    dev_ids = common.dev_ids_to_list(args.dev_ids)
+    socks = []
+    for dev_id in dev_ids:
+        socks.append({
+            'host': '/tmp/sock%d' % dev_id,
+            'guest': '/var/run/usvhost%d' % dev_id})
 
+    # Setup docker command.
     docker_cmd = [
-        'sudo', 'docker', 'run', '-it', '\\',
-        '-v', '%s:%s' % (sock_host, sock_guest), '\\',
+        'sudo', 'docker', 'run', '-it', '\\']
+
+    for sock in socks:
+        docker_cmd += [
+            '-v', '%s:%s' % (sock['host'], sock['guest']), '\\']
+
+    docker_cmd += [
         '-v', '/dev/hugepages:/dev/hugepages', '\\',
         conf.spp_container, '\\'
     ]
 
     cmd_path = 'testpmd'
 
-    testpmd_cmd = [
-        cmd_path, '\\',
+    # Setup testpmd command.
+    testpmd_cmd = [cmd_path, '\\']
+
+    eal_opts = [
         core_opt['attr'], core_opt['val'], '\\',
         '-n', str(args.nof_memchan), '\\',
         mem_opt['attr'], mem_opt['val'], '\\',
-        '--proc-type', 'auto', '\\',
-        '--no-pci', '\\',
-        '--vdev', 'virtio_user%d,path=%s' % (args.dev_id, sock_guest), '\\',
-        '--file-prefix', 'testpmd%d' % args.dev_id, '\\',
-        '--', '\\',
-        '-i', '\\',
-        '--txqflags', '0xf00', '\\',
-        '--disable-hw-vlan'
+        '--proc-type', 'auto', '\\']
+
+    if args.pci is False:
+        eal_opts += ['--no-pci', '\\']
+
+    for i in range(len(dev_ids)):
+        eal_opts += [
+            '--vdev', 'virtio_user%d,path=%s' % (
+                dev_ids[i], socks[i]['guest']), '\\']
+
+    eal_opts += [
+        '--file-prefix', 'testpmd%d' % dev_ids[0], '\\',
+        '--', '\\']
+
+    testpmd_opts = []
+    if args.enable_hw_vlan is True:
+        testpmd_opts += ['--elable-hw-vlan', '\\']
+    else:
+        testpmd_opts += ['--disable-hw-vlan', '\\']
+
+    testpmd_opts += [
+        '-i'
     ]
 
-    cmds = docker_cmd + testpmd_cmd
+    cmds = docker_cmd + testpmd_cmd + eal_opts + testpmd_opts
     common.print_pretty_commands(cmds)
 
+    if args.dry_run is True:
+        exit()
+
+    # Remove delimiters for print_pretty_commands().
     while '\\' in cmds:
         cmds.remove('\\')
     subprocess.call(cmds)
